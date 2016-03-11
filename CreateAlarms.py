91244dc847d13ba709b9eef116bdf42bffaff175
Taylor Higley <Taylor.Higley@afge.org>
2016-03-11 10:49:38 -0500

    Adjusted thresholds and added Try block

--- CreateAlarms.py
+++ CreateAlarms.py
@@ -19,6 +19,7 @@ cw = boto3.client('cloudwatch')
 
 def lambda_handler(event, context):
     
+    
     #Enumerate EC2 instances
     reservations = ec.describe_instances().get('Reservations', [])
     instances = sum(
@@ -28,68 +29,21 @@ def lambda_handler(event, context):
         ], [])
     
     for instance in instances:
-        for tag in instance['Tags']:
-            if tag['Key'] == 'Name':
-                name_tag = tag['Value']
-                print "Found instance %s with name %s" % (instance['InstanceId'], name_tag)
-        
-        #Create Metric "CPU Utilization Greater than 95% for 15+ Minutes"
-        response = cw.put_metric_alarm(
-            AlarmName="%s %s High CPU Utilization Warning" % (name_tag, instance['InstanceId']),
-            AlarmDescription='CPU Utilization Greater than 95% for 15+ Minutes',
-            ActionsEnabled=True,
-            AlarmActions=[
-                ec2_sns,
-            ],
-            MetricName='CPUUtilization',
-            Namespace='AWS/EC2',
-            Statistic='Average',
-            Dimensions=[
-                {
-                    'Name': 'InstanceId',
-                    'Value': instance['InstanceId']
-                },
-            ],
-            Period=300,
-            EvaluationPeriods=3,
-            Threshold=95.0,
-            ComparisonOperator='GreaterThanOrEqualToThreshold'
-        )
-        
-        #Create Metric "CPU Utilization Greater than 95% for 60+ Minutes"
-        response = cw.put_metric_alarm(
-            AlarmName="%s %s High CPU Utilization Critical" % (name_tag, instance['InstanceId']),
-            AlarmDescription='CPU Utilization Greater than 95% for 60+ Minutes',
-            ActionsEnabled=True,
-            AlarmActions=[
-                ec2_sns,
-            ],
-            MetricName='CPUUtilization',
-            Namespace='AWS/EC2',
-            Statistic='Average',
-            Dimensions=[
-                {
-                    'Name': 'InstanceId',
-                    'Value': instance['InstanceId']
-                },
-            ],
-            Period=300,
-            EvaluationPeriods=12,
-            Threshold=95.0,
-            ComparisonOperator='GreaterThanOrEqualToThreshold'
-        )
-        
-        #Create CPU Credit Alarms only on T2 instances
-        if "t2" in instance['InstanceType']:
-            #Create Metric "CPU Credit Balance <= 25 for 30 Minutes"
+        try:
+            for tag in instance['Tags']:
+                if tag['Key'] == 'Name':
+                    name_tag = tag['Value']
+                    print "Found instance %s with name %s" % (instance['InstanceId'], name_tag)
+            
+            #Create Metric "CPU Utilization Greater than 98% for 60+ Minutes"
             response = cw.put_metric_alarm(
-                AlarmName="%s %s Credit Balance Warning" % (name_tag, instance['InstanceId']),
-                AlarmDescription='CPU Credit Balance <= 25 for 30 Minutes',
+                AlarmName="%s %s High CPU Utilization Warning" % (name_tag, instance['InstanceId']),
+                AlarmDescription='CPU Utilization Greater than 95% for 15+ Minutes',
                 ActionsEnabled=True,
                 AlarmActions=[
                     ec2_sns,
                 ],
-                MetricName='CPUCreditBalance',
+                MetricName='CPUUtilization',
                 Namespace='AWS/EC2',
                 Statistic='Average',
                 Dimensions=[
@@ -99,20 +53,20 @@ def lambda_handler(event, context):
                     },
                 ],
                 Period=300,
-                EvaluationPeriods=6,
-                Threshold=25.0,
-                ComparisonOperator='LessThanOrEqualToThreshold'
+                EvaluationPeriods=12,
+                Threshold=98.0,
+                ComparisonOperator='GreaterThanOrEqualToThreshold'
             )
             
-            #Create Metric "CPU Credit Balance <= 5 for 10 Minutes"
+            #Create Metric "CPU Utilization Greater than 98% for 120+ Minutes"
             response = cw.put_metric_alarm(
-                AlarmName="%s %s Credit Balance Critical" % (name_tag, instance['InstanceId']),
-                AlarmDescription='CPU Credit Balance <= 5 for 10 Minutes',
+                AlarmName="%s %s High CPU Utilization Critical" % (name_tag, instance['InstanceId']),
+                AlarmDescription='CPU Utilization Greater than 95% for 60+ Minutes',
                 ActionsEnabled=True,
                 AlarmActions=[
                     ec2_sns,
                 ],
-                MetricName='CPUCreditBalance',
+                MetricName='CPUUtilization',
                 Namespace='AWS/EC2',
                 Statistic='Average',
                 Dimensions=[
@@ -122,59 +76,110 @@ def lambda_handler(event, context):
                     },
                 ],
                 Period=300,
-                EvaluationPeriods=2,
-                Threshold=5.0,
-                ComparisonOperator='LessThanOrEqualToThreshold'
+                EvaluationPeriods=24,
+                Threshold=98.0,
+                ComparisonOperator='GreaterThanOrEqualToThreshold'
             )
+            
+            #Create CPU Credit Alarms only on T2 instances
+            if "t2" in instance['InstanceType']:
+                #Create Metric "CPU Credit Balance <= 25 for 30 Minutes"
+                response = cw.put_metric_alarm(
+                    AlarmName="%s %s Credit Balance Warning" % (name_tag, instance['InstanceId']),
+                    AlarmDescription='CPU Credit Balance <= 25 for 30 Minutes',
+                    ActionsEnabled=True,
+                    AlarmActions=[
+                        ec2_sns,
+                    ],
+                    MetricName='CPUCreditBalance',
+                    Namespace='AWS/EC2',
+                    Statistic='Average',
+                    Dimensions=[
+                        {
+                            'Name': 'InstanceId',
+                            'Value': instance['InstanceId']
+                        },
+                    ],
+                    Period=300,
+                    EvaluationPeriods=6,
+                    Threshold=25.0,
+                    ComparisonOperator='LessThanOrEqualToThreshold'
+                )
+                
+                #Create Metric "CPU Credit Balance <= 5 for 10 Minutes"
+                response = cw.put_metric_alarm(
+                    AlarmName="%s %s Credit Balance Critical" % (name_tag, instance['InstanceId']),
+                    AlarmDescription='CPU Credit Balance <= 5 for 10 Minutes',
+                    ActionsEnabled=True,
+                    AlarmActions=[
+                        ec2_sns,
+                    ],
+                    MetricName='CPUCreditBalance',
+                    Namespace='AWS/EC2',
+                    Statistic='Average',
+                    Dimensions=[
+                        {
+                            'Name': 'InstanceId',
+                            'Value': instance['InstanceId']
+                        },
+                    ],
+                    Period=300,
+                    EvaluationPeriods=2,
+                    Threshold=5.0,
+                    ComparisonOperator='LessThanOrEqualToThreshold'
+                )
+            
+            #Create Metric "Status Check Failed (System) for 5 Minutes"
+            response = cw.put_metric_alarm(
+                AlarmName="%s %s System Check Failed" % (name_tag, instance['InstanceId']),
+                AlarmDescription='Status Check Failed (System) for 5 Minutes',
+                ActionsEnabled=True,
+                AlarmActions=[
+                    ec2_sns,
+                    "arn:aws:automate:%s:ec2:recover" % region,
+                ],
+                MetricName='StatusCheckFailed_System',
+                Namespace='AWS/EC2',
+                Statistic='Average',
+                Dimensions=[
+                    {
+                        'Name': 'InstanceId',
+                        'Value': instance['InstanceId']
+                    },
+                ],
+                Period=60,
+                EvaluationPeriods=5,
+                Threshold=1.0,
+                ComparisonOperator='GreaterThanOrEqualToThreshold'
+            )
+            
+            #Create Metric "Status Check Failed (Instance) for 20 Minutes"
+            response = cw.put_metric_alarm(
+                AlarmName="%s %s Instance Check Failed" % (name_tag, instance['InstanceId']),
+                AlarmDescription='Status Check Failed (Instance) for 20 Minutes',
+                ActionsEnabled=True,
+                AlarmActions=[
+                    ec2_sns,
+                    "arn:aws:swf:%s:%s:action/actions/AWS_EC2.InstanceId.Reboot/1.0" % (region, akid)
+                ],
+                MetricName='StatusCheckFailed_Instance',
+                Namespace='AWS/EC2',
+                Statistic='Average',
+                Dimensions=[
+                    {
+                        'Name': 'InstanceId',
+                        'Value': instance['InstanceId']
+                    },
+                ],
+                Period=60,
+                EvaluationPeriods=20,
+                Threshold=1.0,
+                ComparisonOperator='GreaterThanOrEqualToThreshold'
+            )
+        except Exception, e:
+            print ("Error Encountered.")
+            print (e)
         
-        #Create Metric "Status Check Failed (System) for 5 Minutes"
-        response = cw.put_metric_alarm(
-            AlarmName="%s %s System Check Failed" % (name_tag, instance['InstanceId']),
-            AlarmDescription='Status Check Failed (System) for 5 Minutes',
-            ActionsEnabled=True,
-            AlarmActions=[
-                ec2_sns,
-                "arn:aws:automate:%s:ec2:recover" % region,
-            ],
-            MetricName='StatusCheckFailed_System',
-            Namespace='AWS/EC2',
-            Statistic='Average',
-            Dimensions=[
-                {
-                    'Name': 'InstanceId',
-                    'Value': instance['InstanceId']
-                },
-            ],
-            Period=60,
-            EvaluationPeriods=5,
-            Threshold=1.0,
-            ComparisonOperator='GreaterThanOrEqualToThreshold'
-        )
-        
-        #Create Metric "Status Check Failed (Instance) for 20 Minutes"
-        response = cw.put_metric_alarm(
-            AlarmName="%s %s Instance Check Failed" % (name_tag, instance['InstanceId']),
-            AlarmDescription='Status Check Failed (Instance) for 20 Minutes',
-            ActionsEnabled=True,
-            AlarmActions=[
-                ec2_sns,
-                "arn:aws:swf:%s:%s:action/actions/AWS_EC2.InstanceId.Reboot/1.0" % (region, akid)
-            ],
-            MetricName='StatusCheckFailed_Instance',
-            Namespace='AWS/EC2',
-            Statistic='Average',
-            Dimensions=[
-                {
-                    'Name': 'InstanceId',
-                    'Value': instance['InstanceId']
-                },
-            ],
-            Period=60,
-            EvaluationPeriods=20,
-            Threshold=1.0,
-            ComparisonOperator='GreaterThanOrEqualToThreshold'
-        )
-    
     #Enumerate EBS devices attached to EC2 instances    
     for instance in instances:
         for dev in instance['BlockDeviceMappings']:
@@ -229,7 +234,7 @@ def lambda_handler(event, context):
             Threshold=30.0,
             ComparisonOperator='LessThanOrEqualToThreshold'
         )
-    
+        
     #Enumerate RDS database instances    
     database_instances = rd.describe_db_instances().get('DBInstances', [])
     for database in database_instances:
